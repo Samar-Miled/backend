3688366544e512c2beeb9a2b80cf8d3959a5c63f
samar miled <SAMARMILED864@gmail.com>
2025-02-25 13:41:04 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import path from "path";
+import cors from "cors"
+
 
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
@@ -21,6 +23,7 @@ const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
+app.use(cors())
 app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
 app.use(cookieParser());
 
