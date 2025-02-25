e32e8ac4405bb0839951ceb3266252159a29a3d1
samar miled <SAMARMILED864@gmail.com>
2025-02-25 13:46:48 +0100

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,11 @@ const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
-app.use(cors())
+app.use(cors({
+  origin: 'https://frontend-1xsr.onrender.com', // Remplace par l'URL exacte de ton frontend
+  credentials: true // Autorise l'envoi de cookies et des credentials
+}));
+
 app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
 app.use(cookieParser());
 
